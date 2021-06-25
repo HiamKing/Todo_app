037cea7898a761b0daa8acb83d248f685826f3cc
Vu Hieu <vuminhhieu1312@gmail.com>
2021-06-25 22:16:15 +0700

    chinh lai font

--- frontend/static/frontend/js/list.js
+++ frontend/static/frontend/js/list.js
@@ -1,24 +1,3 @@
-/*
-    KEY COMPONENTS:
-    "activateItem" = null until an edit button is clicked. Will contain
-    object of item we are editing
-    "list_snapshot" = Will contain previous state of list. Used for
-    removing extra rows on list update
-
-    PROCESS:
-    1 - Fetch Data and build rows "buildList()"
-    2 - Create Item on form submit
-    3 - Edit Item click - Prefill form and change submit URL
-    4 - Delete Item - Send Item id  to delete URL
-    5 - Cross out completed task - Event handle updated item
-
-    NOTES:
-    -- Add event handlers to "edit", "delete", "title"
-    -- Render with strike through items completed
-    -- Remove extra data on re-render
-    -- CSRF Token
-*/
-
 var user_id = localStorage.getItem('user_id')
 
 function buildList() {
--- frontend/views.py
+++ frontend/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
 from django.views import View
-# Create your views here.
 
+
+# Create your views here.
 def task_list_view(request):
     return render(request, 'frontend/list.html')
 
@@ -9,8 +10,10 @@ def open_view(request):
     return render(request, 'frontend/open.html')
 
 def register_view(request):
+
     return render(request, 'frontend/register.html')
 
 def login_view(request):
+
     return render(request, 'frontend/login.html')
 
