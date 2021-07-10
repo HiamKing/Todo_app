1edfe6bbf3ececeae06669effc3c11b4a186f185
Vu Hieu <vuminhhieu1312@gmail.com>
2021-07-10 16:35:25 +0700

    update db

--- todo_app/settings.py
+++ todo_app/settings.py
@@ -12,6 +12,10 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import os
+import dj_database_url
+
+db_from_env = dj_database_url.config()
+DATABASES['default'].update(db_from_env)
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -80,7 +84,7 @@ WSGI_APPLICATION = 'todo_app.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
+        'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'todo_app',
         'USER': 'hiamking',
         'PASSWORD': 'laffincoffin1',
