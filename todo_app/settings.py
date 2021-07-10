fd5b9ca79012480060d4fd7f1157fc9c8ee461b3
Vu Hieu <vuminhhieu1312@gmail.com>
2021-07-10 16:38:26 +0700

    chinh lai db config

--- todo_app/settings.py
+++ todo_app/settings.py
@@ -14,9 +14,6 @@ from pathlib import Path
 import os
 import dj_database_url
 
-db_from_env = dj_database_url.config()
-DATABASES['default'].update(db_from_env)
-
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -91,6 +88,8 @@ DATABASES = {
     }
 }
 
+db_from_env = dj_database_url.config()
+DATABASES['default'].update(db_from_env)
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
