2c9189f2540132fe8176727d3ef8dfc166069745
Vu Hieu <vuminhhieu1312@gmail.com>
2021-07-10 15:31:30 +0700

    chinh lai settings

--- todo_app/settings.py
+++ todo_app/settings.py
@@ -128,10 +128,6 @@ STATIC_URL = '/static/'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
-
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
